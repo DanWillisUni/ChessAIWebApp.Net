fd9d2778c42a301b97e92a332449ec7e41a27bff
DanWillisUni <dan.willis.uni@gmail.com>
2020-11-21 11:32:49 +0000

    first commit

---
+++ ChessAIWebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ChessAIWebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
